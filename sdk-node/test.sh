9c47f30b1d0be72c9390f5c917433b0ea4a92509
LuatCoi97 <mabongdem123@gmail.com>
2018-10-02 05:30:37 +0700

    edit file test.sh

--- sdk-node/test.sh
+++ sdk-node/test.sh
@@ -1,2 +1,5 @@
- 
-node invoke.js -u user97 --channel mychannel --chaincode mycc1 -m initResult  -a "112" -a "2222" -a "Nam" -a "3333" -a "toan" -a "4444" -a "hung" -a "Gioi" -a "20172" -l "8"
+rm latency.txt
+rm throughput.txt
+
+LOOP = $1
+node invoke.js -u user97 --channel mychannel --chaincode mycc1 -m initResult  -a "112" -a "2222" -a "Nam" -a "3333" -a "toan" -a "4444" -a "hung" -a "Gioi" -a "20172" -l "$LOOP"
