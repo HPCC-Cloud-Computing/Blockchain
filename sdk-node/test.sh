6e0ed1048707c8607a8d72d6b924e6fd6f9ad867
LuatCoi97 <mabongdem123@gmail.com>
2018-10-05 16:09:23 +0700

    edit file test6

--- sdk-node/test.sh
+++ sdk-node/test.sh
@@ -1,9 +1,9 @@
 rm latency.txt
 rm throughput.txt
 
-ID1=$1
+ID=$1
 LOOP=$2
 for i in 1..$LOOP; do
-    node invoke.js -u user97 --channel mychannel --chaincode mycc1 -m initResult  -a "$ID1" -a "2222" -a "Nam" -a "3333" -a "toan" -a "4444" -a "hung" -a "Gioi" -a "20172"
-    ID1 = "$ID1$i";
+    node invoke.js -u user97 --channel mychannel --chaincode mycc1 -m initResult  -a "$ID" -a "2222" -a "Nam" -a "3333" -a "toan" -a "4444" -a "hung" -a "Gioi" -a "20172"
+    ID="$ID$i";
 done
