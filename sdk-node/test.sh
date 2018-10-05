4195533a9a145f82b28a73720f7727abe99deeeb
LuatCoi97 <mabongdem123@gmail.com>
2018-10-05 16:07:04 +0700

    edit file test5

--- sdk-node/test.sh
+++ sdk-node/test.sh
@@ -3,7 +3,7 @@ rm throughput.txt
 
 ID1=$1
 LOOP=$2
-for i in 0..$LOOP; do
+for i in 1..$LOOP; do
     node invoke.js -u user97 --channel mychannel --chaincode mycc1 -m initResult  -a "$ID1" -a "2222" -a "Nam" -a "3333" -a "toan" -a "4444" -a "hung" -a "Gioi" -a "20172"
-    $ID1 = $ID1 + "0";
+    ID1 = "$ID1$i";
 done
---
+++ sdk-node/ttt.sh
@@ -0,0 +1,5 @@
+test=$1
+for i in 1; do
+test="$test$i"
+done
+echo $test
