b3844170205d2cf218ad6591f4afd10c2f285b8b
LuatCoi97 <mabongdem123@gmail.com>
2018-10-09 00:16:47 +0700

    edit file test_query

--- sdk-node/test.sh
+++ sdk-node/test.sh
@@ -4,5 +4,3 @@ rm throughput.txt
 ID=$1
 LOOP=$2
 node invoke.js -u user97 --channel mychannel --chaincode mycc1 -m initResult  -a "$ID" -a "2222" -a "Nam" -a "3333" -a "toan" -a "4444" -a "hung" -a "Gioi" -a "20172" -l "$LOOP"
-
-java read
