8ede50744fc43979a63434ee57f178d8bc0fc784
LuatCoi97 <mabongdem123@gmail.com>
2018-10-09 13:42:04 +0700

    edit file test_query1

--- sdk-node/query.js
+++ sdk-node/query.js
@@ -44,7 +44,7 @@ var request = {
     fcn: program.method,
     args: program.arguments
 };
-var timeWait = 900;
+var timeWait = 70;
 invoke();
 async function invoke() {
     for (var i = 0; i < 2 * numLoop; i++) {
